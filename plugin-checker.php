0119f923e7f7af8fe9a4329a6fb835953d614129
Death302 <aaditya.uzumaki@gmail.com>
2024-07-13 17:48:34 +0530

    Update 2

--- plugin-checker.php
+++ plugin-checker.php
@@ -1,11 +1,17 @@
 <?php
 /*
 Plugin Name: Unused Plugins and Themes Checker
-Description: A plugin to identify unused plugins and themes in a WordPress multisite network.
-Version: 1.1
+Description: A plugin to identify and delete unused plugins and themes in a WordPress multisite network.
+Version: 1.2
 Author: Aaditya Uzumaki 
 Website: https://goenka.xyz
 */
+
+// Enqueue Bootstrap CSS
+add_action('admin_enqueue_scripts', 'uptc_enqueue_styles');
+function uptc_enqueue_styles() {
+    wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+}
 
 // Hook to add a menu item in the network admin menu
 add_action('network_admin_menu', 'uptc_add_menu_page');
@@ -21,6 +27,18 @@ function uptc_add_menu_page() {
         20
     );
 }
+
+// Function to delete selected plugins
+function uptc_delete_plugins() {
+    if (isset($_POST['delete_plugins']) && isset($_POST['unused_plugins'])) {
+        $plugins_to_delete = $_POST['unused_plugins'];
+        foreach ($plugins_to_delete as $plugin) {
+            delete_plugins([$plugin]);
+        }
+    }
+}
+
+add_action('admin_init', 'uptc_delete_plugins');
 
 function uptc_display_unused_plugins_themes() {
     global $wpdb;
@@ -65,59 +83,75 @@ function uptc_display_unused_plugins_themes() {
 
     // Active plugins table
     echo '<h2>Active Plugins</h2>';
-    echo '<table class="widefat fixed" cellspacing="0">';
-    echo '<thead><tr><th>Plugin</th><th>Sites</th></tr></thead>';
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th>Plugin</th><th>Sites</th><th>Count</th></tr></thead>';
     echo '<tbody>';
     foreach ($all_plugins as $plugin_file => $plugin_data) {
         if (in_array($plugin_file, $used_plugins)) {
-            echo '<tr>';
-            echo '<td>' . esc_html($plugin_data['Name']) . ' (' . esc_html($plugin_file) . ')</td>';
-            echo '<td>';
+            $count = 0;
             $sites_using_plugin = array();
             foreach ($active_plugins as $blog_id => $plugins) {
                 if (in_array($plugin_file, $plugins)) {
                     $sites_using_plugin[] = get_blog_details($blog_id)->blogname;
+                    $count++;
                 }
             }
-            echo implode(', ', $sites_using_plugin);
-            echo '</td>';
+            echo '<tr>';
+            echo '<td>' . esc_html($plugin_data['Name']) . ' (' . esc_html($plugin_file) . ')</td>';
+            echo '<td>' . implode(', ', $sites_using_plugin) . '</td>';
+            echo '<td>' . esc_html($count) . '</td>';
             echo '</tr>';
         }
     }
     echo '</tbody></table>';
 
-    // Unused plugins table
+    // Unused plugins table with deletion option
     echo '<h2>Unused Plugins</h2>';
     if (!empty($unused_plugins)) {
-        echo '<table class="widefat fixed" cellspacing="0">';
-        echo '<thead><tr><th>Plugin</th></tr></thead>';
+        echo '<form method="post">';
+        echo '<table class="table table-striped">';
+        echo '<thead><tr><th>Plugin</th><th>Select</th></tr></thead>';
         echo '<tbody>';
+        $counter = 0;
         foreach ($unused_plugins as $plugin) {
-            echo '<tr><td>' . esc_html($all_plugins[$plugin]['Name']) . ' (' . esc_html($plugin) . ')</td></tr>';
+            echo '<tr>';
+            echo '<td>' . esc_html($all_plugins[$plugin]['Name']) . ' (' . esc_html($plugin) . ')</td>';
+            echo '<td><input type="checkbox" name="unused_plugins[]" value="' . esc_attr($plugin) . '"></td>';
+            echo '</tr>';
+            $counter++;
+            if ($counter % 20 == 0) {
+                echo '<tr><td colspan="2">';
+                echo '<button type="submit" name="delete_plugins" class="btn btn-danger">Delete Selected Plugins</button>';
+                echo '</td></tr>';
+            }
         }
         echo '</tbody></table>';
+        echo '<button type="submit" name="delete_plugins" class="btn btn-danger">Delete Selected Plugins</button>';
+        echo '</form>';
     } else {
         echo '<p>All plugins are in use.</p>';
     }
 
     // Active themes table
     echo '<h2>Active Themes</h2>';
-    echo '<table class="widefat fixed" cellspacing="0">';
-    echo '<thead><tr><th>Theme</th><th>Sites</th></tr></thead>';
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th>Theme</th><th>Sites</th><th>Count</th><th>Parent Theme</th></tr></thead>';
     echo '<tbody>';
     foreach ($all_themes as $theme_slug => $theme) {
         if (in_array($theme_slug, $used_themes)) {
-            echo '<tr>';
-            echo '<td>' . esc_html($theme->get('Name')) . ' (' . esc_html($theme_slug) . ')</td>';
-            echo '<td>';
+            $count = 0;
             $sites_using_theme = array();
             foreach ($active_themes as $blog_id => $theme_info) {
                 if ($theme_info['stylesheet'] == $theme_slug) {
                     $sites_using_theme[] = get_blog_details($blog_id)->blogname;
+                    $count++;
                 }
             }
-            echo implode(', ', $sites_using_theme);
-            echo '</td>';
+            echo '<tr>';
+            echo '<td>' . esc_html($theme->get('Name')) . ' (' . esc_html($theme_slug) . ')</td>';
+            echo '<td>' . implode(', ', $sites_using_theme) . '</td>';
+            echo '<td>' . esc_html($count) . '</td>';
+            echo '<td>' . esc_html($theme->parent() ? $theme->parent()->get('Name') : 'None') . '</td>';
             echo '</tr>';
         }
     }
@@ -126,7 +160,7 @@ function uptc_display_unused_plugins_themes() {
     // Unused themes table
     echo '<h2>Unused Themes</h2>';
     if (!empty($unused_themes)) {
-        echo '<table class="widefat fixed" cellspacing="0">';
+        echo '<table class="table table-striped">';
         echo '<thead><tr><th>Theme</th></tr></thead>';
         echo '<tbody>';
         foreach ($unused_themes as $theme) {
